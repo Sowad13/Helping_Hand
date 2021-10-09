8ef535ece672ec85f5be7690a5a82b8c36a84308
nushoRaat <nushrat.j01@gmail.com>
2021-10-09 19:33:30 +0600

    Sign UP

--- app/src/main/java/com/example/helpinghand/MainActivity.java
+++ app/src/main/java/com/example/helpinghand/MainActivity.java
@@ -2,13 +2,73 @@ package com.example.helpinghand;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonStart;
+
+//    FirebaseAuth mAuth;
+//    private static int SPLASH_SCREEN = 3000;
+//    Animation topanim;
+//    ImageView logoimg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        setContentView( R.layout.activity_main );
+
         setContentView(R.layout.activity_main);
+
+        buttonStart= findViewById(R.id.button);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( MainActivity.this,SignUp.class );
+                startActivity( intent );
+                finish();
+            }
+        });
+
+
+//
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        topanim = AnimationUtils.loadAnimation(this,R.anim.top_anim);
+//
+//        logoimg = findViewById( R.id.logo );
+//        logoimg.setAnimation( topanim );
+//
+//
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                // if user don't logout then user will sent to dashbord every time user opens the app
+//                if(mAuth.getCurrentUser()!=null)
+//                {
+//                    Intent intent= new Intent(MainActivity.this,NavigationPage.class);
+//                    startActivity(intent);
+//                    finish();
+//
+//                }
+//                else {
+//                    Intent intent = new Intent(MainActivity.this,SignIn.class);
+//                    startActivity(intent);
+//                    finish();
+//                }
+//            }
+//        },SPLASH_SCREEN);
     }
 }
--- app/src/main/java/com/example/helpinghand/SignIn.java
+++
@@ -1,8 +0,0 @@
-package com.example.helpinghand;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class SignIn extends AppCompatActivity {
-
-
-}
